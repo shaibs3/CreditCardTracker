d94a629b00bc5d31b5e4821f3a0c798b2ccfb5bc
shaibs3 <shaibs3@gmail.com>
2019-02-20 14:26:39 +0200

    add firebase console

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: "AIzaSyBNP87C1fwM5oXDyfTRRT14LBPPec_lBPQ",
+  authDomain: "creditcardtracker-73574.firebaseapp.com",
+  databaseURL: "https://creditcardtracker-73574.firebaseio.com",
+  projectId: "creditcardtracker-73574",
+  storageBucket: "creditcardtracker-73574.appspot.com",
+  messagingSenderId: "143118282833"
 };
